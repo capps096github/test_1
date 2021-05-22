4ce311295d59e482bbe023394d9b559e8c1bb01f
capps096github <capps096github@gmail.com>
2021-05-23 01:54:08 +0800

    FirstCommit

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello Cephas! Github is Here to Try");
+
+printf("If others can then why can't  Cephas Brian");
+
+printf("Try me today");
+
+printf("Lets do this together");
+
+printf("Things are getting interesting");
+
+//Xephas BRian Codes are here
+    return 0;
+}
